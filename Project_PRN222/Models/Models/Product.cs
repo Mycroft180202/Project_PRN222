ff3f13ee1729f3b54fbce506d70f09804ad4e4c6
Nguyễn Sông La <109718216+KiooooSllaa@users.noreply.github.com>
2025-03-09 13:45:29 +0700

    Add files via upload

---
+++ Project_PRN222/Models/Models/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Project_PRN222.Models;
+
+public partial class Product
+{
+    public int ProductId { get; set; }
+
+    public string ProductName { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public decimal Price { get; set; }
+
+    public int StockQuantity { get; set; }
+
+    public string? ImageUrl { get; set; }
+
+    public int? CategoryId { get; set; }
+
+    public DateTime? CreatedDate { get; set; }
+
+    public DateTime? UpdatedDate { get; set; }
+
+    public virtual ICollection<CartItem> CartItems { get; set; } = new List<CartItem>();
+
+    public virtual Category? Category { get; set; }
+
+    public virtual ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+
+    public virtual ICollection<SalesStatistic> SalesStatistics { get; set; } = new List<SalesStatistic>();
+}
---
+++ Project_PRN222/Models/Models/ProjectPrn222Context.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Project_PRN222.Models;
+
+public partial class ProjectPrn222Context : DbContext
+{
+    public ProjectPrn222Context()
+    {
+    }
+
+    public ProjectPrn222Context(DbContextOptions<ProjectPrn222Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<CartItem> CartItems { get; set; }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Delivery> Deliveries { get; set; }
+
+    public virtual DbSet<Order> Orders { get; set; }
+
+    public virtual DbSet<OrderItem> OrderItems { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<Role> Roles { get; set; }
+
+    public virtual DbSet<SalesStatistic> SalesStatistics { get; set; }
+
+    public virtual DbSet<ShipmentMethod> ShipmentMethods { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var ConnectionString = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<CartItem>(entity =>
+        {
+            entity.HasKey(e => e.CartItemId).HasName("PK__CartItem__488B0B0AEFA9B7DC");
+
+            entity.Property(e => e.AddedDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+
+            entity.HasOne(d => d.Product).WithMany(p => p.CartItems)
+                .HasForeignKey(d => d.ProductId)
+                .HasConstraintName("FK__CartItems__Produ__4AB81AF0");
+
+            entity.HasOne(d => d.User).WithMany(p => p.CartItems)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK__CartItems__UserI__49C3F6B7");
+        });
+
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.CategoryId).HasName("PK__Categori__19093A0B8291B046");
+
+            entity.HasIndex(e => e.CategoryName, "UQ__Categori__8517B2E0300DDC61").IsUnique();
+
+            entity.Property(e => e.CategoryName).HasMaxLength(100);
+            entity.Property(e => e.Description).HasMaxLength(255);
+
+            entity.HasOne(d => d.ParentCategory).WithMany(p => p.InverseParentCategory)
+                .HasForeignKey(d => d.ParentCategoryId)
+                .HasConstraintName("FK__Categorie__Paren__4222D4EF");
+        });
+
+        modelBuilder.Entity<Delivery>(entity =>
+        {
+            entity.HasKey(e => e.DeliveryId).HasName("PK__Deliveri__626D8FCEF55F4B6C");
+
+            entity.HasIndex(e => e.OrderId, "UQ__Deliveri__C3905BCE89D9682A").IsUnique();
+
+            entity.Property(e => e.CreatedDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.DeliveryDate).HasColumnType("datetime");
+            entity.Property(e => e.DeliveryStatus).HasMaxLength(50);
+            entity.Property(e => e.ShippingAddress).HasMaxLength(255);
+            entity.Property(e => e.TrackingCode).HasMaxLength(100);
+            entity.Property(e => e.UpdatedDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+
+            entity.HasOne(d => d.Order).WithOne(p => p.Delivery)
+                .HasForeignKey<Delivery>(d => d.OrderId)
+                .HasConstraintName("FK__Deliverie__Order__5CD6CB2B");
+
+            entity.HasOne(d => d.ShipmentMethod).WithMany(p => p.Deliveries)
+                .HasForeignKey(d => d.ShipmentMethodId)
+                .HasConstraintName("FK__Deliverie__Shipm__5DCAEF64");
+        });
+
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.HasKey(e => e.OrderId).HasName("PK__Orders__C3905BCFD60EC89B");
+
+            entity.Property(e => e.BillingAddress).HasMaxLength(255);
+            entity.Property(e => e.CreatedDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.OrderDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.OrderStatus).HasMaxLength(50);
+            entity.Property(e => e.PaymentMethod).HasMaxLength(50);
+            entity.Property(e => e.ShippingAddress).HasMaxLength(255);
+            entity.Property(e => e.TotalAmount).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.UpdatedDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+
+            entity.HasOne(d => d.ShipmentMethod).WithMany(p => p.Orders)
+                .HasForeignKey(d => d.ShipmentMethodId)
+                .HasConstraintName("FK__Orders__Shipment__534D60F1");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Orders)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK__Orders__UserId__5165187F");
+        });
+
+        modelBuilder.Entity<OrderItem>(entity =>
+        {
+            entity.HasKey(e => e.OrderItemId).HasName("PK__OrderIte__57ED0681E3879F51");
+
+            entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.ProductName).HasMaxLength(255);
+
+            entity.HasOne(d => d.Order).WithMany(p => p.OrderItems)
+                .HasForeignKey(d => d.OrderId)
+                .HasConstraintName("FK__OrderItem__Order__5812160E");
+
+            entity.HasOne(d => d.Product).WithMany(p => p.OrderItems)
+                .HasForeignKey(d => d.ProductId)
+                .HasConstraintName("FK__OrderItem__Produ__59063A47");
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.ProductId).HasName("PK__Products__B40CC6CDC4BD7E68");
+
+            entity.Property(e => e.CreatedDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.ImageUrl).HasMaxLength(255);
+            entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.ProductName).HasMaxLength(255);
+            entity.Property(e => e.UpdatedDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+
+            entity.HasOne(d => d.Category).WithMany(p => p.Products)
+                .HasForeignKey(d => d.CategoryId)
+                .HasConstraintName("FK__Products__Catego__44FF419A");
+        });
+
+        modelBuilder.Entity<Role>(entity =>
+        {
+            entity.HasKey(e => e.RoleId).HasName("PK__Roles__8AFACE1A1DD121BF");
+
+            entity.HasIndex(e => e.RoleName, "UQ__Roles__8A2B61600AE69A92").IsUnique();
+
+            entity.Property(e => e.Description).HasMaxLength(255);
+            entity.Property(e => e.RoleName).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<SalesStatistic>(entity =>
+        {
+            entity.HasKey(e => e.StatisticId).HasName("PK__SalesSta__367DEB176DC6E2FB");
+
+            entity.Property(e => e.TimeDimension).HasMaxLength(20);
+            entity.Property(e => e.TotalRevenue).HasColumnType("decimal(18, 2)");
+
+            entity.HasOne(d => d.Category).WithMany(p => p.SalesStatistics)
+                .HasForeignKey(d => d.CategoryId)
+                .HasConstraintName("FK__SalesStat__Categ__6383C8BA");
+
+            entity.HasOne(d => d.Product).WithMany(p => p.SalesStatistics)
+                .HasForeignKey(d => d.ProductId)
+                .HasConstraintName("FK__SalesStat__Produ__628FA481");
+        });
+
+        modelBuilder.Entity<ShipmentMethod>(entity =>
+        {
+            entity.HasKey(e => e.ShipmentMethodId).HasName("PK__Shipment__5F8180BD10AC3A4C");
+
+            entity.HasIndex(e => e.MethodName, "UQ__Shipment__218CFB17DF7F1947").IsUnique();
+
+            entity.Property(e => e.Cost).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.Description).HasMaxLength(255);
+            entity.Property(e => e.MethodName).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.UserId).HasName("PK__Users__1788CC4C4B0B43AF");
+
+            entity.HasIndex(e => e.Email, "UQ__Users__A9D105347BCF8E3B").IsUnique();
+
+            entity.HasIndex(e => e.UserName, "UQ__Users__C9F28456D73586A7").IsUnique();
+
+            entity.Property(e => e.Address).HasMaxLength(255);
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.IsActive).HasDefaultValue(true);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+            entity.Property(e => e.PasswordHash)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.PhoneNumber)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.RegistrationDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.UserName).HasMaxLength(50);
+
+            entity.HasOne(d => d.Role).WithMany(p => p.Users)
+                .HasForeignKey(d => d.RoleId)
+                .HasConstraintName("FK__Users__RoleId__3E52440B");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
